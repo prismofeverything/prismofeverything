8ee090ae7d2b3bc00541834bae68841adb85a28c
Ryan Spangler <ryan.spangler@gmail.com>
2017-10-22 15:02:48 -0700

    prism of everything

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject prismofeverything "0.0.1"
+  :description "prism of everything"
+  :url "http://prismofeverything.com"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
---
+++ src/prismofeverything/core.clj
@@ -0,0 +1,3 @@
+(ns prismofeverything.core)
+
+
